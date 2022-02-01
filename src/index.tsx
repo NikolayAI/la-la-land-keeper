fbdaf0163a7a19f55a014bb34fff6e16be5df786
NikolayAI <kodi_kodi_kodi@mail.ru>
2022-02-01 22:57:58 +0500

    add products entity model tests

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,3 @@ import ReactDOM from 'react-dom';
 import App from './app/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-export { useTimer } from './shared/lib/useTimer';
-export { IUseTimer } from './shared/lib/useTimer';
