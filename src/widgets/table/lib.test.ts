67b351e8cd658e53a83ed6ff30e2595cd17932b5
NikolayAI <kodi_kodi_kodi@mail.ru>
2022-03-02 18:10:21 +0500

    add table widget lib tests

---
+++ src/widgets/table/lib.test.ts
@@ -0,0 +1,1 @@
+import { calculateTableTotalPrice } from './lib';import { table } from '../../../tests/__mocks__/handlers/fixtures';test('calculateTableTotalPrice should return value', async () => {  const result = calculateTableTotalPrice(table);  expect(result).toBe(1);});
