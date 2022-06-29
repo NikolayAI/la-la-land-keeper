92ca238d6af6ea0e0743d07b5231d1d038612e0b
NikolayAI <kodi_kodi_kodi@mail.ru>
2022-06-29 20:39:24 +0500

    fix theme color setter

--- src/entities/computed/settings/model/model.ts
+++ src/entities/computed/settings/model/model.ts
@@ -2,11 +2,15 @@ import { createEffect, createStore, forward } from 'effector';
 
 import { ColorThemes } from '@/shared';
 
-export const setColorThemeFx = createEffect<ColorThemes, void>();
+export const setColorThemeFx = createEffect<ColorThemes, ColorThemes>();
 
 export const $colorTheme = createStore<ColorThemes>(ColorThemes.light);
 
+setColorThemeFx.use((theme) => {
+  return theme;
+});
+
 forward({
-  from: setColorThemeFx,
+  from: setColorThemeFx.doneData,
   to: $colorTheme,
 });
